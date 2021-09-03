7b6c15c4651f8754b56464051aa5c1df74081e51
Melz8bit <37233103+Melz8bit@users.noreply.github.com>
2021-09-03 19:29:45 -0400

    Adjustments and fixes

--- price_scraper.py
+++ price_scraper.py
@@ -7,29 +7,25 @@ import os
 
 FILE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
-game_title = ''
-price_list = {}
-lowest_prices_list = {}
-
-def send_email():
+""" def send_email():
     email_from = 'melz8bit@gmail.com'
     email_to = 'melz8bit@gmail.com'
 
     with open(os.path.join(FILE_DIRECTORY, 'prices.txt'), 'r') as f:
         msg = MIMEText(f.read())
-    
+
     msg['Subject'] = f'{game_title} - Prices as of {date.today}'
     msg['From'] = f'{email_from}'
     msg['To'] = f'{email_to}'
 
     s = smtplib.SMTP('localhost')
     s.sendmail(email_from, email_to, msg.as_string())
-    s.quit()
+    s.quit() """
 
 
-def save_prices_to_file():
+def save_prices_to_file(game_title, price_list, lowest_prices_list):
     with open(os.path.join(FILE_DIRECTORY, 'prices.txt'), 'w') as f:
-        f.write(f'{game_title.strip()}')
+        f.write(f'{game_title}\n\n')
 
         f.write('All Time Lowest Prices\n')
         f.write('----------------------\n')
@@ -41,29 +37,35 @@ def save_prices_to_file():
         for store, price in price_list.items():
             f.write(f'{store}:\n\t\t\t{price}\n')
 
+
 def get_prices():
+    price_list = {}
+    lowest_prices_list = {}
+
     url = 'https://www.dekudeals.com/items/tales-of-vesperia-definitive-edition'
 
     result = requests.get(url)
     doc = BeautifulSoup(result.text, 'html.parser')
 
     game_title = doc.find('title').text
-    
-    prices = doc.find_all(text='Current prices')[0].find_all_next('img', alt=True)
+
+    prices = doc.find_all(text='Current prices')[
+        0].find_all_next('img', alt=True)
     for img in prices:
         if img['alt'] != 'Screenshot' and img['alt'] != '' and img['alt'] is not None:
-            
+
             game_version = img.find_next('td', class_='version').text
 
             # Physical version
             for price in img.find_all_next('div', class_='btn btn-block btn-outline-secondary'):
                 price_list[f'{img["alt"].strip()} ({game_version.strip()})'] = f'{price.text.split("-", 1)[0].strip()}'
-                
+
             # Digital version
             for price in img.find_all_next('div', class_='btn btn-block btn-primary'):
                 price_list[f'{img["alt"].strip()} ({game_version.strip()})'] = f'{price.text.split("-", 1)[0].strip()}'
 
-    all_time_low = doc.find_all('strong', text='All time low')[0].find_all_next('td', colspan=True)
+    all_time_low = doc.find_all('strong', text='All time low')[
+        0].find_all_next('td', colspan=True)
     for td in all_time_low:
         game_version = td.text.strip()
 
@@ -72,13 +74,20 @@ def get_prices():
         else:
             lowest_prices_list[f'{game_version}'] = f'{td.find_next("td", class_=True).find_next("td", class_=True).text.strip()}'
 
+    return(game_title, price_list, lowest_prices_list)
+
+
 def main():
-    
-    get_prices()
-    
-    save_prices_to_file()
-    
-    send_email()
+
+    game_title = ''
+    price_list = {}
+    lowest_prices_list = {}
+
+    game_title, price_list, lowest_prices_list = get_prices()
+    save_prices_to_file(game_title, price_list, lowest_prices_list)
+
+    # send_email()
+
 
 if __name__ == '__main__':
     main()
